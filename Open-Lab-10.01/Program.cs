9b5870b359d1cf413bac27bf86e9f4aad3c1ecd8
janosheen <91180541+janosheen@users.noreply.github.com>
2021-11-25 11:19:57 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_10._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.title = "Najlepsia kniha";
+            Console.WriteLine(LOTR.title);
+        }
+        class Book
+        {
+            public string title;
+            private string pages;
+        }
+    }
+}
