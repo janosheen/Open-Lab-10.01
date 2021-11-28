8ffd5453cd15d5b4586cd915db985ec1cf53409b
janosheen <91180541+janosheen@users.noreply.github.com>
2021-11-28 13:53:44 +0100

    Open-Lab-10.02 (I modified the previous object-oriented "Book" exercise and created new variables that will write themselves into console.)

--- Open-Lab-10.01/Program.cs
+++ Open-Lab-10.01/Program.cs
@@ -7,13 +7,33 @@ namespace Open_Lab_10._01
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "Najlepsia kniha";
-            Console.WriteLine(LOTR.title);
+            LOTR.Názov("Najlepšia kniha");
+            LOTR.Žáner("SciFi");
+            LOTR.Vydanie("prosinec");
+            LOTR.Autor("Yanko");
+            LOTR.Strany(12);
         }
         class Book
         {
-            public string title;
-            private string pages;
+            private string title;
+            private string cathegory;
+            private string releaseDate;
+            private string author;
+            private int pages;
+            public void Názov(string novýNázov) { title = novýNázov; }
+            public void Žáner(string novýŽáner) { cathegory = novýŽáner; }
+            public void Vydanie(string novéVydanie) { releaseDate = novéVydanie; }
+            public void Autor(string novýAutor) { author = novýAutor; }
+            public void Strany(int novéStrany) { pages = novéStrany; }
+
+            public void NapíšPremenné()
+            {
+                Console.WriteLine("Názov: " + title);
+                Console.WriteLine("Počet strán: " + pages);
+                Console.WriteLine("Žáner: " + cathegory);
+                Console.WriteLine("Autor: " + author);
+                Console.WriteLine("Dátum vydania: " + releaseDate);
+            }
         }
     }
 }
